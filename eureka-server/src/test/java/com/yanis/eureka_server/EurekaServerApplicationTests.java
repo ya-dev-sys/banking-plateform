92020b6461b7ca5d50e91951480fe3c7d920c028
yanis <finagnonallade@gmail.com>
2025-12-08 10:37:07 +0100

    feat: Add Eureka Server, Config Server, API Gateway configuration, and Docker Compose setup.

---
+++ eureka-server/src/test/java/com/yanis/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanis.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
